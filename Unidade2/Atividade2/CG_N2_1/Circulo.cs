a52d59b92d206f7beb60470fb9e9bebfdb3d9f7e
Gabriel Panca Ribeiro <gpribeiro@furb.br>
2023-03-31 21:55:15 -0300

    atividade 1 sem classe circulo e começo atividade 2

---
+++ Unidade2/Atividade2/CG_N2_1/Circulo.cs
@@ -0,0 +1,14 @@
+using gcgcg;
+
+public class Circulo {
+    public Circulo(int espacoBolinhas, double raio)
+    {
+        // var circulo = new Objeto('A', null);
+        // // for (int i = 0; i < 360; i+=10)
+        // // {
+        //     var ponto = Matematica.GerarPtosCirculo(50, 1);
+        //     objetoNovo.PontosAdicionar(ponto);
+        // // }
+        // circulo.Atualizar();
+    }
+}
